7cf7dee114e871d22bf29e6e9e44e43d71929189
Grant Sweeney <grantsweeney02@gmail.com>
2024-02-14 12:51:15 -0500

    edited some images

--- js/script.js
+++ js/script.js
@@ -7,3 +7,22 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
         });
     });
 });
+
+function changeFaviconForColorScheme() {
+    const faviconElement = document.querySelector('link[rel="icon"]');
+    const darkModeMediaQuery = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+    );
+
+    if (darkModeMediaQuery.matches) {
+        faviconElement.href = "../assests/images/dark-mode-favicon.ico";
+    } else {
+        faviconElement.href = "../assests/images/light-mode-favicon.ico";
+    }
+    console.log("Changing favicon to:", faviconElement.href);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    changeFaviconForColorScheme();
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', changeFaviconForColorScheme);
+  });
