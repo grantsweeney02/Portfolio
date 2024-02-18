e2c6972c8d953d089dcb01d57cb6eb6e77f3cec6
Grant Sweeney <grantsweeney02@gmail.com>
2024-02-18 15:05:33 -0500

    changing icon now works!

--- js/script.js
+++ js/script.js
@@ -1,19 +1,8 @@
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-    anchor.addEventListener("click", function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior: "smooth",
-        });
-    });
-});
-
 function changeFaviconForColorScheme() {
     const faviconElement = document.querySelector('link[rel="icon"]');
     const darkModeMediaQuery = window.matchMedia(
         "(prefers-color-scheme: dark)"
     );
-
     if (darkModeMediaQuery.matches) {
         faviconElement.href = "../assests/images/dark-mode-favicon.ico";
     } else {
@@ -22,7 +11,9 @@ function changeFaviconForColorScheme() {
     console.log("Changing favicon to:", faviconElement.href);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
     changeFaviconForColorScheme();
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', changeFaviconForColorScheme);
-  });
+    window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .addEventListener("change", changeFaviconForColorScheme);
+});
