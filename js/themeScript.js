5445053b9215f0a7a659c8b4e18f05fb67e2f996
Grant Sweeney <grantsweeney02@gmail.com>
2024-03-07 19:59:43 -0500

    revamp

--- js/themeScript.js
+++ js/themeScript.js
@@ -1,7 +1,6 @@
 function updateFaviconBasedOnColorScheme() {
     const faviconElement = document.querySelector('link[rel="icon"]');
     const colorSchemeQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    console.log("System in Dark Mode:", colorSchemeQuery.matches);
     faviconElement.href = colorSchemeQuery.matches
         ? "../assets/images/dark-mode-favicon.ico"
         : "../assets/images/light-mode-favicon.ico";
