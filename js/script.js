2835cc00e0cd99ff103c26fa1065a76c1927b0a4
Grant Sweeney <grantsweeney02@gmail.com>
2024-03-10 23:29:23 -0400

    finished first draft

--- js/script.js
+++ js/script.js
@@ -1,16 +1,41 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const sections = document.querySelectorAll('.section');
-    const navLinks = document.querySelectorAll('.nav-link');
+document.addEventListener("DOMContentLoaded", (event) => {
+    const sections = document.querySelectorAll(".section");
+    const navLinks = document.querySelectorAll(".nav-link");
 
     function changeLinkState() {
         let index = sections.length;
 
-        while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+        while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
 
-        navLinks.forEach((link) => link.classList.remove('active'));
-        navLinks[index].classList.add('active');
+        navLinks.forEach((link) => link.classList.remove("active"));
+        navLinks[index].classList.add("active");
     }
 
     changeLinkState();
-    window.addEventListener('scroll', changeLinkState);
+    window.addEventListener("scroll", changeLinkState);
+});
+
+
+// work experience card carousel
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector(".cards-container");
+    const cards = container.querySelectorAll(".card");
+    const cardWidth = cards[0].offsetWidth + 20;
+    const visibleCards = 3;
+    let maxScroll = (cards.length - visibleCards) * cardWidth;
+    let currentScroll = 0;
+
+    document.querySelector(".next").addEventListener("click", () => {
+        if (currentScroll < maxScroll) {
+            currentScroll += cardWidth * visibleCards;
+            container.scrollBy({ left: cardWidth * visibleCards, top: 0, behavior: "smooth" });
+        }
+    });
+
+    document.querySelector(".prev").addEventListener("click", () => {
+        if (currentScroll > 0) {
+            currentScroll -= cardWidth * visibleCards;
+            container.scrollBy({ left: -cardWidth * visibleCards, top: 0, behavior: "smooth" });
+        }
+    });
 });
